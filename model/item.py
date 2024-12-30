cdc2ae0d77790a50f2d8910429e1be39107baf2f
sr9000 <no@mail.org>
2024-12-30 15:13:07 +0300

    remove unused functions

--- model/item.py
+++ model/item.py
@@ -22,23 +22,6 @@ def wrap_primitive(value: bool | float | str, level: int) -> Item:
     return Item(value=value, level=level)
 
 
-def wrap_bool(value: bool, level: int) -> Item:
-    return Item(value=value, level=level)
-
-
-def wrap_float(value: float, level: int) -> Item:
-    return Item(value=value, level=level)
-
-
-def wrap_str(value: str, level: int) -> Item:
-    return Item(value=value, level=level)
-
-
-def wrap_int(value: int, level: int) -> Item:
-    """force cast to float under the hood"""
-    return Item(value=float(value), level=level)
-
-
 def wrap_list(level: int) -> Item:
     return Item(value=[], level=level)
 
