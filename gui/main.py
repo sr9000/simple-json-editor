bf5a21934c8aa804f5b3a493af8dd12238706ae1
sr9000 <no@mail.org>
2025-01-10 20:39:07 +0300

    nested data

--- gui/main.py
+++ gui/main.py
@@ -2,6 +2,7 @@ from PySide6.QtWidgets import QApplication, QMainWindow
 import sys
 
 from gui.window import Ui_MainWindow
+from gui.model import TreeModel, TreeItem
 
 
 def main():
@@ -14,6 +15,26 @@ def main():
     window_ui = Ui_MainWindow()
     window_ui.setupUi(window)
 
+    headers = ["Column 1", "Number"]
+    data = [
+        ["Item 1", 1, [["SubItem 1.1", 1.1], ["SubItem 1.2", 1.2]]],
+        ["Item 2", 2, [["SubItem 2.1", 2.1]]],
+        ["Item 3", 3],
+    ]
+
+    def add_items(parent, elements):
+        for element in elements:
+            item = TreeItem(element[:2], parent)
+            parent.appendChild(item)
+            if len(element) > 2:
+                add_items(item, element[2])
+
+    root_item = TreeItem(headers)
+    add_items(root_item, data)
+
+    model = TreeModel(headers, [])
+    model.rootItem = root_item
+    window_ui.treeView.setModel(model)
 
     # Show window
     window.show()
---
+++ gui/model.py
@@ -0,0 +1,95 @@
+from PySide6.QtCore import QAbstractItemModel, QModelIndex, Qt
+
+
+class TreeItem:
+    def __init__(self, data, parent=None):
+        self.parentItem = parent
+        self.itemData = data
+        self.childItems = []
+
+    def appendChild(self, item):
+        self.childItems.append(item)
+
+    def child(self, row):
+        return self.childItems[row]
+
+    def childCount(self):
+        return len(self.childItems)
+
+    def columnCount(self):
+        return len(self.itemData)
+
+    def data(self, column):
+        return self.itemData[column]
+
+    def parent(self):
+        return self.parentItem
+
+    def row(self):
+        if self.parentItem:
+            return self.parentItem.childItems.index(self)
+        return 0
+
+
+class TreeModel(QAbstractItemModel):
+    def __init__(self, headers, data, parent=None):
+        super(TreeModel, self).__init__(parent)
+        self.rootItem = TreeItem(headers)
+        self.setupModelData(data, self.rootItem)
+
+    def columnCount(self, parent=QModelIndex()):
+        if parent.isValid():
+            return parent.internalPointer().columnCount()
+        return self.rootItem.columnCount()
+
+    def data(self, index, role):
+        if not index.isValid():
+            return None
+        if role != Qt.DisplayRole:
+            return None
+        item = index.internalPointer()
+        return item.data(index.column())
+
+    def flags(self, index):
+        if not index.isValid():
+            return Qt.NoItemFlags
+        return Qt.ItemIsEnabled | Qt.ItemIsSelectable
+
+    def headerData(self, section, orientation, role):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return self.rootItem.data(section)
+        return None
+
+    def index(self, row, column, parent=QModelIndex()):
+        if not self.hasIndex(row, column, parent):
+            return QModelIndex()
+        if not parent.isValid():
+            parentItem = self.rootItem
+        else:
+            parentItem = parent.internalPointer()
+        childItem = parentItem.child(row)
+        if childItem:
+            return self.createIndex(row, column, childItem)
+        return QModelIndex()
+
+    def parent(self, index):
+        if not index.isValid():
+            return QModelIndex()
+        childItem = index.internalPointer()
+        parentItem = childItem.parent()
+        if parentItem == self.rootItem:
+            return QModelIndex()
+        return self.createIndex(parentItem.row(), 0, parentItem)
+
+    def rowCount(self, parent=QModelIndex()):
+        if parent.column() > 0:
+            return 0
+        if not parent.isValid():
+            parentItem = self.rootItem
+        else:
+            parentItem = parent.internalPointer()
+        return parentItem.childCount()
+
+    def setupModelData(self, data, parent):
+        for item in data:
+            parent.appendChild(TreeItem(item, parent))
